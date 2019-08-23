9eec5c088fcead4264e59f329f6cd95e27d888da
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-23 02:40:16 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -1,3 +1,17 @@
 var index = require("./index.js")
 
+// if (name === "Susan") {
+//     console.log("The name is correct")
+// } else {
+//     console.log("Expected: Susan, Received: "+index.name)
+// }
+
+
+
+// if (height === 70) {
+//     console.log("The height is correct")
+// } else {
+//     console.log("Expected: 70, Received: "+index.height)
+// }
+
 console.log(index)
