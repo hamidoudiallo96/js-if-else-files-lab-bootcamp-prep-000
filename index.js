20b043812cb2362477831cb085f840991c7b3a06
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-23 02:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ module.exports = { name, height
 
 if (name === "Susan") {
     console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
-}
-
-
-
-if (height === 70) {
+    if (height === 70) {
     console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
-}
+  } 
+} 
+
+
+
+
 
